553607304623eeeff65cb5802afb49a8d1a01477
shoomankhatri <shoomankhatri@gmail.com>
2026-05-18 21:51:11 +0545

    feat: add rest apis for todo app

---
+++ todo_api/views.py
@@ -0,0 +1,17 @@
+# from django.shortcuts import render
+
+# Create your views here.
+
+from rest_framework import generics
+from .models import Todo
+from .serializers import TodoSerializer
+
+class TodoListCreateView(generics.ListCreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+class TodoDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+
